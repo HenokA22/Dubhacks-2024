b81c8d330cef2d53ca8f7276f91dabb246fc0d64
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-10-12 18:11:00 -0700

    globe shows up in dashboard

--- music-map/src/routes/Dashboard.jsx
+++ music-map/src/routes/Dashboard.jsx
@@ -1,11 +1,32 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 import Navbar from '../components/Navbar'
+import '../css/Dashboard.css'
 
 function Dashboard () {
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic3VuaXRpYSIsImEiOiJjbTI2djc1ejQwZWZwMmxwdjExOWh5b3JyIn0.X81TiGsk7gesz7bXYXv2SA'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
   return (
-    <div>
+    <div id="screen">
       <Navbar />
-      <h1>Dashboard</h1>
-      <p>Insert your map here</p>
+      <h2>Dashboard</h2>
+      <div id="map-container" ref={mapContainerRef}>
+      </div>
     </div>
   )
 }
